e9410a3aa8c2890adcc631f4527122e2ac360897
Paul Thomas <paulthomas2107@gmail.com>
2020-11-01 23:21:31 +0000

    More....

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,55 +2,35 @@ import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 
 class Counter extends Component {
-  state = {
-    count: 0,
-    tags: ["tag1", "tag2", "tag3"],
-  };
-
-  formatCount() {
-    return this.state.count === 0 ? "Zero" : this.state.count;
+  render() {
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.props.onIncrement(this.props.counter)}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
+      </div>
+    );
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
-  handleIncrement = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
-
-  renderTags() {
-    if (this.state.tags.length === 0) {
-      return <p>No tags found....</p>;
-    } else {
-      return (
-        <div>
-          <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-          <button
-            onClick={this.handleIncrement}
-            className="btn btn-secondary btn-sm"
-          >
-            Increment{" "}
-          </button>
-          <ul>
-            {this.state.tags.map((tag) => (
-              <li key={tag}>{tag}</li>
-            ))}
-          </ul>
-        </div>
-      );
-    }
-  }
-
-  render() {
-    return (
-      <div>
-        {this.state.tags.length === 0 && "Please create a new tag."}
-        {this.renderTags()}
-      </div>
-    );
+  formatCount() {
+    const { value } = this.props.counter;
+    return value === 0 ? "Zero" : value;
   }
 }
 
